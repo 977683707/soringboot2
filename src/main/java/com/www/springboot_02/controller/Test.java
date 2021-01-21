8d9cc4245267f6234ac8eccf21cb8f8a1000bbcb
www <977683707@qq.com>
2021-01-21 17:21:04 +0800

    修改Test

--- src/main/java/com/www/springboot_02/controller/Test.java
+++ src/main/java/com/www/springboot_02/controller/Test.java
@@ -1,4 +1,11 @@
 package com.www.springboot_02.controller;
 
 public class Test {
+    public static void main(String[] args) {
+
+        System.out.printf("asjda");
+        System.out.printf("asjda");
+        System.out.printf("asjda");
+        System.out.printf("asjda");
+    }
 }
